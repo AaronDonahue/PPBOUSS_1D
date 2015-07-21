dc42b6f9c45a9bbbbd3043170e551b28b5df1d07
Diaplo <adonahu1@nd.edu>
2015-07-21 10:52:58 -0400

    Added the git version tracker to makefile

---
+++ src/version.F90
@@ -0,0 +1,10 @@
+      SUBROUTINE version()
+
+      IMPLICIT NONE
+      
+      PRINT*, "Version Information"
+      PRINT*, "  Branch: master" 
+      PRINT*, "  SHA: 941a572db9f65247a8b5a1e0a7983b47535e46a8     +" 
+      PRINT*, " "
+ 
+      END SUBROUTINE version
